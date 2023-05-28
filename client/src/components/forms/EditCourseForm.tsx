0d54129d2a9a38e6e1b42dcf37cbeae628eaba7e
victormunozcalzada <victormunozcalzada@gmail.com>
2023-05-29 00:22:24 +0200

    minor changes edit course form

--- client/src/components/forms/EditCourseForm.tsx
+++ client/src/components/forms/EditCourseForm.tsx
@@ -88,11 +88,9 @@ const EditCourseForm: React.FC<Props> = ({
           ))}
         </select>
       </div>
-      <div className="flex items-center justify-center w-full mb-6">
-        <label
-          htmlFor="dropzone-file"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-        >
+      <div className="mb-6">
+        <label htmlFor="file_input" className="block font-medium text-gray-700">
+          Imagen
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
             <svg
               aria-hidden="true"
@@ -103,9 +101,9 @@ const EditCourseForm: React.FC<Props> = ({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
               ></path>
             </svg>
@@ -117,7 +115,13 @@ const EditCourseForm: React.FC<Props> = ({
               SVG, PNG, JPG or GIF (MAX. 600x400px)
             </p>
           </div>
-          <input id="dropzone-file" type="file" className="hidden" />
+          <input
+            id="file_input"
+            type="file"
+            name="img"
+            className="hidden"
+            onChange={handleInputChange}
+          />
         </label>
       </div>
       <div className="mb-6">
@@ -151,7 +155,7 @@ const EditCourseForm: React.FC<Props> = ({
           type="button"
           onClick={() => handleSave(editCourse.id ?? 0)}
         >
-          Save
+          Guardar
         </button>
       </div>
     </form>
